78b8d51e62b4b2a4b6314b376e8be8ad67a0d954
Temiloluwa Onaleye <onaleyetemmy@outlook.com>
2022-04-26 10:45:25 +0100

    airbnb-clone

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./Footer";
 
 import Header from "./Header";
 import Home from "./Home";
+import SearchPage from "./SearchPage";
 
 function App() {
   //BEM
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -4,14 +4,18 @@ import SearchIcon from "@mui/icons-material/Search";
 import LanguageIcon from "@mui/icons-material/Language";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Avatar from "@mui/material/Avatar";
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__icon"
-        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-        alt="airbnb-logo"
-      />
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+          alt="airbnb-logo"
+        />
+      </Link>
+
       <div className="header__center">
         <input type="text" />
         <SearchIcon />
